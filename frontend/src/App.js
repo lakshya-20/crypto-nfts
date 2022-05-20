64212d2e84eb9cccf1864f177744fd3cabb9faac
Lakshya Bansal <bansallakshya94128@gmail.com>
2022-05-20 20:16:51 +0530

    Side nav with balance and minting feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,17 @@ function App() {
   return (
     <div className='App'>
       <AuthContextProvider>
-        <div className = "row d-flex">
-          <div className="col-3 border">
-            <SideNav/>
+        <div className = "d-flex">
+          <div className="col-2">
+            <div className='col-2 fixed-top'>
+              <SideNav/>
+            </div>
           </div>
-          <div className="col-9 border">
-            <div className='col-12'>
+          <div className="col-10 border">
+            <div className='col-12 sticky-top'>
               <TopNav/>
             </div>
-            <div className='col-12'>
+            <div className='col-12 border'>
               <Main/>
             </div>
           </div>
--- frontend/src/Layouts/SideNav/index.js
+++ frontend/src/Layouts/SideNav/index.js
@@ -1,18 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { authStateIncreaseGoldCoin, authStateIncreaseSilverCoin } from "../../Services/Actions/AuthActionCreator";
 import { AuthContext } from "../../Services/Contexts/AuthContext";
+import '../../Assets/Styles/SideNav.Layout.css';
+import { BiCoin } from "react-icons/bi";
+import { BsStars } from "react-icons/bs"
+import {InputGroup, Input, InputGroupText} from 'reactstrap';
 
 const SideNav = () => {
   const {authState, authDispatch} = useContext(AuthContext);
 
   const buySilver = async (amount) => {
     if(authState.coinContract){
+      amount = parseInt(amount);
       try{
         const response = await authState.coinContract.methods.buySilver(amount).send({
           from: authState.address,
           value: window.web3.utils.toWei(((1/100)*amount).toString(), 'ether'),
         });
         authDispatch(authStateIncreaseSilverCoin(amount));
+        document.getElementById("silver_coin_input").value = "";
       } catch(err){
         console.log(err.message);
       }
@@ -21,12 +27,14 @@ const SideNav = () => {
 
   const buyGold = async (amount) => {
     if(authState.coinContract){
+      amount = parseInt(amount);
       try{
         const response = await authState.coinContract.methods.buyGold(amount).send({
           from: authState.address,
           value: window.web3.utils.toWei(((1/10)*amount).toString(), 'ether'),
         });
         authDispatch(authStateIncreaseGoldCoin(amount));
+        document.getElementById("gold_coin_input").value = "";
       } catch(err){
         console.log(err.message);
       }
@@ -35,15 +43,77 @@ const SideNav = () => {
 
 
   return (
-    <div>
-      Side Nav Layout
-      <div>
-        Silver Coins: {authState.silverCoins}
-        <button onClick={() => buySilver(10)}>Mint Silver</button>
+    <div className="wrapper">
+      <div className="user-wrapper">
+        <div className="d-flex align-items-center justify-content-between">
+          <img 
+            src="https://res.cloudinary.com/dstmsi8qv/image/upload/v1589896899/c1lfv7nmmjva48wia4a7.png" 
+            width="50px"
+            className="user-image"
+          />
+          <span>
+            <span className="user-name">
+              Lakshya Bansal
+            </span>
+            <br/>
+            <span className="user-label">
+              NFTer
+            </span>
+          </span>
+          <span className="user-badge">
+            Pro+
+          </span>
+        </div>
       </div>
-      <div>
-        Gold Coins: {authState.goldCoins}
-        <button onClick={() => buyGold(1)}>Mint Gold</button>
+      <div className="balance-wrapper">
+        <div className="balance-card">
+          <span className="balance-heading">
+            Your Balance
+          </span>
+          <p>
+            <span className="balance-amount">
+              {authState.silverCoins}
+            </span>
+            <br/>
+            <span className="balance-title d-flex align-items-center justify-content-center">
+              <BiCoin className="balance-icon balance-silver-icon"/> &nbsp; Silver
+            </span>
+          </p>
+          <p>
+            <span className="balance-amount">
+              {authState.goldCoins}
+            </span>
+            <br/>
+            <span className="balance-title d-flex align-items-center justify-content-center">
+            <BiCoin className="balance-icon balance-gold-icon"/> &nbsp; Gold
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="top-up-wrapper">
+        <div className="top-up-heading d-flex align-items-center">
+          <BsStars/>&nbsp;&nbsp;<span>Top Up Balance</span>
+        </div>
+        <br/>
+        <div>
+          <div className="">
+            <InputGroup>
+              <Input placeholder="Silver" id="silver_coin_input"/>
+              <InputGroupText onClick={() => buySilver(document.getElementById("silver_coin_input").value)} type="button">
+                Mint
+              </InputGroupText>
+            </InputGroup>
+            <hr/>
+            <InputGroup>
+              <Input placeholder="Gold" id="gold_coin_input"/>
+              <InputGroupText onClick={() => buyGold(document.getElementById("gold_coin_input").value)} type="button">
+                Mint
+              </InputGroupText>
+            </InputGroup>
+          </div>
+        </div>
+        {/* <button onClick={() => buySilver(10)}>Mint Silver</button>
+        <button onClick={() => buyGold(1)}>Mint Gold</button> */}
       </div>
     </div>
   )
